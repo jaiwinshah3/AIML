7781d994ede5d2b73d452c0eca7cb7956c920c16
jaiwinshah3 <72293443+jaiwinshah3@users.noreply.github.com>
2021-11-30 12:54:08 +0530

    n

--- exp1/text_to_speech.py
+++ exp1/text_to_speech.py
@@ -4,7 +4,7 @@ import os
   
 mytext = 'Machine learning (ML) is the study of computer algorithms that can improve automatically through experience and by the use of data.'
   
-language = 'fr'
+language = 'en'
   
 
 myobj = gTTS(text=mytext, lang=language, slow=False) 
