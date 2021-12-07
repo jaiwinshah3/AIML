fd1b121da3380deae3eb1574a1a3a95f1655278b
jaiwinshah3 <72293443+jaiwinshah3@users.noreply.github.com>
2021-12-07 12:48:32 +0530

    AIML-Lab

---
+++ project/weather .py
@@ -0,0 +1,156 @@
+
+
+import numpy as np 
+import pandas as pd 
+# for visualizations
+import plotly.express as px 
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from datetime import datetime # Time Series analysis.
+df = pd.read_csv(r"C:\Users\jaiwi\Desktop\Weather.csv")
+
+df.head()
+
+df = df.drop('Unnamed: 0', 1)
+
+df
+
+#making an attribute that would contain date (month, year). So that we could get temperature values with the timeline.
+df1 = pd.melt(df, id_vars='YEAR', value_vars=df.columns[1:]) ## melting the data
+df1.head() 
+
+
+df1['Date'] = df1['variable'] + ' ' + df1['YEAR'].astype(str)  
+df1.loc[:,'Date'] = df1['Date'].apply(lambda x : datetime.strptime(x, '%b %Y')) ## Converting String to datetime object
+df1.head()
+
+
+df1.columns=['Year', 'Month', 'Temprature', 'Date']
+df1.sort_values(by='Date', inplace=True) ## To get the time series right.
+fig = go.Figure(layout = go.Layout(yaxis=dict(range=[0, df1['Temprature'].max()+1])))
+fig.add_trace(go.Scatter(x=df1['Date'], y=df1['Temprature']), )
+fig.update_layout(title='Temprature Throught Timeline:',
+                 xaxis_title='Time', yaxis_title='Temprature in Degrees')
+fig.update_layout(xaxis=go.layout.XAxis(
+    rangeselector=dict(
+        buttons=list([dict(label="Whole View", step="all"),
+                      dict(count=1,label="One Year View",step="year",stepmode="todate")                      
+                     ])),
+        rangeslider=dict(visible=True),type="date")
+)
+fig.show()
+
+
+#Insights:
+#May 1921 has been the hottest month in india in history.
+#Dec, Jan and Feb are the coldest months(“Winter”)
+#Apr, May, Jun, July and Aug are the hottest months.(“Summer”).
+
+fig = px.box(df1, 'Month', 'Temprature')
+fig.update_layout(title='Warmest, Coldest and Median Monthly Tempratue.')
+fig.show()
+
+#July is the month with least Standard Deviation. 
+#So,temperature in July vary least. We can expect any day in July to be a warm day.
+
+from sklearn.cluster import KMeans
+sse = []
+target = df1['Temprature'].to_numpy().reshape(-1,1)
+num_clusters = list(range(1, 10))
+
+for k in num_clusters:
+    km = KMeans(n_clusters=k)
+    km.fit(target)
+    sse.append(km.inertia_)
+
+fig = go.Figure(data=[
+    go.Scatter(x = num_clusters, y=sse, mode='lines'),
+    go.Scatter(x = num_clusters, y=sse, mode='markers')
+])
+
+fig.update_layout(title="Evaluation on number of clusters:",
+                 xaxis_title = "Number of Clusters:",
+                 yaxis_title = "Sum of Squared Distance",
+                 showlegend=False)
+fig.show()
+
+km = KMeans(3)
+km.fit(df1['Temprature'].to_numpy().reshape(-1,1))
+df1.loc[:,'Temp Labels'] = km.labels_
+fig = px.scatter(df1, 'Date', 'Temprature', color='Temp Labels')
+fig.update_layout(title = "Temprature clusters.",
+                 xaxis_title="Date", yaxis_title="Temprature")
+fig.show()
+
+# we can see 3 main clusters based on temperatures.
+# Jan, Feb and Dec are the coldest months.
+# Apr, May, Jun, Jul, Aug and Sep; all have hotter temperatures.
+# Mar, Oct and Nov are the months that have temperatures neither too hot nor too cold.
+
+fig = px.histogram(x=df1['Temprature'], nbins=200, histnorm='density')
+fig.update_layout(title='Frequency chart of temprature readings:',
+                 xaxis_title='Temprature', yaxis_title='Count')
+
+# There is a cluster from 26.2-27.5 and mean temperature for most months during history has been between 26.8-26.9
+
+df['Yearly Mean'] = df.iloc[:,1:].mean(axis=1) ## Axis 1 for row wise and axis 0 for columns.
+fig = go.Figure(data=[
+    go.Scatter(name='Yearly Tempratures' , x=df['YEAR'], y=df['Yearly Mean'], mode='lines'),
+    go.Scatter(name='Yearly Tempratures' , x=df['YEAR'], y=df['Yearly Mean'], mode='markers')
+])
+fig.update_layout(title='Yearly Mean Temprature :',
+                 xaxis_title='Time', yaxis_title='Temprature in Degrees')
+fig.show()
+
+fig = px.line(df1, 'Year', 'Temprature', facet_col='Month', facet_col_wrap=4)
+fig.update_layout(title='Monthly temprature throught history:')
+fig.show()
+
+df['Winter'] = df[['DEC', 'JAN', 'FEB']].mean(axis=1)
+df['Summer'] = df[['MAR', 'APR', 'MAY']].mean(axis=1)
+df['Monsoon'] = df[['JUN', 'JUL', 'AUG', 'SEP']].mean(axis=1)
+df['Autumn'] = df[['OCT', 'NOV']].mean(axis=1)
+seasonal_df = df[['YEAR', 'Winter', 'Summer', 'Monsoon', 'Autumn']]
+seasonal_df = pd.melt(seasonal_df, id_vars='YEAR', value_vars=seasonal_df.columns[1:])
+seasonal_df.columns=['Year', 'Season', 'Temprature']
+
+fig = px.scatter(seasonal_df, 'Year', 'Temprature', facet_col='Season', facet_col_wrap=2, trendline='ols')
+fig.update_layout(title='Seasonal mean tempratures throught years:')
+fig.show()
+
+px.scatter(df1, 'Month', 'Temprature', size='Temprature', animation_frame='Year')
+
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import train_test_split 
+from sklearn.metrics import r2_score 
+
+df2 = df1[['Year', 'Month', 'Temprature']].copy()
+df2 = pd.get_dummies(df2)
+y = df2[['Temprature']]
+x = df2.drop(columns='Temprature')
+
+dtr = DecisionTreeRegressor()
+train_x, test_x, train_y, test_y = train_test_split(x,y,test_size=0.3)
+dtr.fit(train_x, train_y)
+pred = dtr.predict(test_x)
+r2_score(test_y, pred)
+
+next_Year = df1[df1['Year']==2017][['Year', 'Month']]
+next_Year.Year.replace(2017,2018, inplace=True)
+next_Year= pd.get_dummies(next_Year)
+temp_2018 = dtr.predict(next_Year)
+
+temp_2018 = {'Month':df1['Month'].unique(), 'Temprature':temp_2018}
+temp_2018=pd.DataFrame(temp_2018)
+temp_2018['Year'] = 2018
+temp_2018
+
+
+forecasted_temp = pd.concat([df1,temp_2018], sort=False).groupby(by='Year')['Temprature'].mean().reset_index()
+fig = go.Figure(data=[
+    go.Scatter(name='Yearly Mean Temprature', x=forecasted_temp['Year'], y=forecasted_temp['Temprature'], mode='lines'),
+    go.Scatter(name='Yearly Mean Temprature', x=forecasted_temp ['Year'], y=forecasted_temp['Temprature'], mode='markers')
+])
+fig.update_layout(title='Forecasted Temprature:',
+                 xaxis_title='Time', yaxis_title='Temprature in Degrees')
+fig.show()
